402d1a0fb9493349c842a2b2119fc9d0970f6af9
Jared Song <jskyejet@gmail.com>
2022-01-28 18:26:09 +0800

    Make working OCR for bytes

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -1,28 +1,17 @@
 from PIL import Image
-from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image import convert_from_bytes
 import pytesseract
 import magic
 
+from io import BytesIO
+import uuid
 import os
-import sys
-import pathlib
-import tempfile
 
 import errors
 
 class PDFReader(object):
-    def __init__(self, pdf_filename: str, pdf_bytes: bytes, is_file: bool = True) -> None:
-        self._PDF_BYTES = None
-        self._FILENAME = "temp.pdf"
-        self._is_file = is_file
-
-        if is_file:
-            try:
-                self.validate_pdf_bytes(pdf_filename)
-            except FileNotFoundError as e:
-                print(f"Error: {e}")
-        else:
-            self._PDF_BYTES = pdf_bytes
+    def __init__(self, pdf_bytes: bytes) -> None:
+        self._PDF_BYTES = pdf_bytes
 
         try:
             self.validate_pdf_bytes()
@@ -31,30 +20,21 @@ class PDFReader(object):
 
         self.out_text = self.process_pdf()
 
+
     def validate_pdf_bytes(self):
-        if not (self._PDF_BYTES and "pdf" in magic.from_buffer(self._PDF_BYTES)):
-            raise errors.InvalidPDFFile("PDF file is not valid")
+        return self._PDF_BYTES and "pdf" in magic.from_buffer(self._PDF_BYTES, mime=True)
 
-    def pdf_file_exists(self, pdf_filename):
-        self._FILENAME = pdf_filename
-        filepath = pathlib.Path(self._FILENAME)
-
-        if filepath.is_file():
-            with open(filepath, "rb") as f:
-                self._PDF_BYTES = f.read()
-        else:
-            raise FileNotFoundError(f"PDF file {self._FILENAME} not found.")
 
     def process_pdf(self) -> list:
         out_text = []
 
-        if self._is_file:
-            pages = convert_from_path(self._FILENAME, fmt="jpg")
-        else:
-            pages = convert_from_bytes(self._PDF_BYTES, fmt="jpg")
+        pages = convert_from_bytes(self._PDF_BYTES, fmt="jpg")
             
-        for idx, img in enumerate(pages):
-            text = str(pytesseract.image_to_string(img))
+        for img in pages:
+            b = BytesIO()
+            img.save(b, "JPEG")
+            text = str(pytesseract.image_to_string(Image.open(b)))
+            b.close()
             text = text.replace('-\n', '')
             out_text.append(text)
 
@@ -66,5 +46,17 @@ class PDFReader(object):
         return self.out_text
 
         
+def main():
+    try:
+        with open("/home/woohoo/msprofile.pdf", "rb") as in_pdf:
+            data = in_pdf.read()
+        print(data[:10])
+        reader = PDFReader(data)
+        print(reader.text)
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
 
+
+if __name__ == '__main__':
+    main()
         
