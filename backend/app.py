8f97dd41a186819648f3149fb43cee27a84c9c31
Zhang Zeyu <zhangzeyu2001@gmail.com>
2022-03-30 00:22:23 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -20,6 +20,13 @@ def get_words(in_bytes: bytes) -> list:
 
 app = Flask(__name__)
 
+@app.after_request 
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    # Other headers can be added here if required
+    return response
+
 @app.route("/health", methods=["GET"])
 def get_health():
     return "Hello"
